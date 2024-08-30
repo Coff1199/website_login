5a4bd6d78f0a73efe4fb666ceafb4c16aac97163
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-08-30 16:09:33 -0400

    sent info to backend

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -6,16 +6,32 @@ function Login() {
     const [showPass, setShowPass] = useState(false);
     return (
         <>
-            <form id="login-form">
+            <form id="login-form" onSubmit={ (e) => {
+                    e.preventDefault();
+                    if (username && password) {
+                        console.log("Success, " + username + " " + password);
+                        fetch('/api/login', {
+                            headers: {'Content-Type': 'application/json'},
+                            method: "POST",
+                            body: JSON.stringify({username: username, password: password})
+                        }).then((res) => {
+
+                        })
+                        setUsername('');
+                        setPassword('');
+                    } else {
+                        console.log("empty")
+                    }
+                }}>
                 <div>
                     <label htmlFor="user">Username:</label>
-                    <input id="user"type="text" onChange={(e) => {
+                    <input id="user"type="text" value= {username} onChange={(e) => {
                         setUsername(e.target.value)
                     }}/>
                 </div>
                 <div>
                     <label htmlFor="pass">Password:</label>
-                    <input id="pass" type={showPass? "text" : "password"} onChange={(e) => {
+                    <input id="pass" value={password} type={showPass? "text" : "password"} onChange={(e) => {
                         setPassword(e.target.value)
                     }}/>
                 </div>
@@ -23,11 +39,7 @@ function Login() {
                         setShowPass(!showPass);
                     }}/>
                     <label htmlFor="show-pass-checkbox">Show Password?</label>
-                <input type="button" value="Login" onClick={ () => {
-                    console.log("Success, " + username + " " + password);
-                    setUsername('');
-                    setPassword('');
-                }}/>
+                <input type="submit" value="Login" />
             </form>
         </>
     )
