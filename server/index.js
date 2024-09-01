0190120c2527388d30b70ea19714caa2658c94ca
Christian Offenheiser <118592069+Coff1199@users.noreply.github.com>
2024-09-01 13:09:21 -0400

    added self signed certificate for dev environment

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require('cors');
-
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
 const PORT = 3002;
 
 const app = express();
 app.use(express.json())
 app.use(cors())
+
+const options = {
+  key: fs.readFileSync(path.resolve(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.resolve(__dirname, 'cert.pem')),
+};
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
